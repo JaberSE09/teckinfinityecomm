0a12ebbe9ada8b12d34161e530debdefbf304b6c
Sajid Jaber <sajidjaber@yahoo.com>
2024-02-29 18:30:07 -0600

    fixed the header

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,4 +1,6 @@
 'use client'
+
+import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -9,8 +11,10 @@ import { Gutter } from '../../Gutter'
 import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
+
 const HeaderComponent = ({ header }: { header: Header }) => {
   const pathname = usePathname()
+
   return (
     <nav
       className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
@@ -18,15 +22,10 @@ const HeaderComponent = ({ header }: { header: Header }) => {
         .join(' ')}
     >
       <Gutter className={classes.wrap}>
-        <Link href={'/'}>
-          <Image
-            width={'170'}
-            height={'50'}
-            src={'/logo-black.svg'}
-            alt="logo"
-            className={classes.logo}
-          />
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
         </Link>
+
         <HeaderNav header={header} />
       </Gutter>
     </nav>
